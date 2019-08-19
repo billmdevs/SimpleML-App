e9f55a8b00cfc976a9fde23e9bf75e5b782caef5
Bill Morrisson <billmorrissonjr@gmail.com>
2019-08-19 16:18:49 -0400

    Come up with display of the front end and fix buttons

--- front/src/index.js
+++ front/src/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
+import App from './App';
+//import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Use register() if you want to load the app offline and faster and unregister()
+// if you want to use the app online.
+//serviceWorker.unregister();
